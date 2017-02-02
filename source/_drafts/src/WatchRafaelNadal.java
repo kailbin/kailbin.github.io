6b905d4340e34c7a65c3ba14b607102fe43a07b2
kail <kail@MrKail.local>
2017-02-02 23:22:21 +0800

    src

---
+++ source/_drafts/src/WatchRafaelNadal.java
@@ -0,0 +1,44 @@
+/**
+ * Created by kail on 2017/2/2.
+ */
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.WatchEvent.Kind;
+
+public class WatchRafaelNadal {
+
+    public void watchRNDir(Path path) throws IOException, InterruptedException {
+        try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
+            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
+
+            //start an infinite loop
+            while (true) {
+
+                //retrieve and remove the next watch key
+                final WatchKey key = watchService.take();
+
+                //get list of pending events for the watch key
+                for (WatchEvent<?> watchEvent : key.pollEvents()) {
+                    //get the kind of event (create, modify, delete)
+                    final Kind<?> kind = watchEvent.kind();
+                    //handle OVERFLOW event
+                    if (kind == StandardWatchEventKinds.OVERFLOW) {
+                        continue;
+                    }
+                    //get the filename for the event
+                    final WatchEvent<Path> watchEventPath = (WatchEvent<Path>) watchEvent;
+                    final Path filename = watchEventPath.context();
+                    //print it out
+                    System.out.println(kind + " -> " + filename);
+                }
+                //reset the key
+                boolean valid = key.reset();
+                //exit loop if the key is not valid (if the directory was deleted, for example)
+                if (!valid) {
+                    break;
+                }
+            }
+        }
+    }
+}
